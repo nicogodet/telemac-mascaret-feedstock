f00cb86c7d879820aebf99a665f399fa2316b8e0
nicogodet <nicolas.godet@outlook.fr>
2024-04-02 16:45:12 +0200

    Remove doc and examples

--- recipe/build-documentation.bat
+++
@@ -1,3 +0,0 @@
-:: Copy documentation
-mkdir %LIBRARY_PREFIX%\opentelemac\documentation
-xcopy %SRC_DIR%\opentelemac\documentation\*.pdf %LIBRARY_PREFIX%\opentelemac\documentation /E /H /C /I
--- recipe/build-examples.bat
+++
@@ -1,5 +0,0 @@
-:: Copy examples ad notebooks
-mkdir %LIBRARY_PREFIX%\opentelemac\examples
-mkdir %LIBRARY_PREFIX%\opentelemac\notebooks
-xcopy %SRC_DIR%\opentelemac\examples %LIBRARY_PREFIX%\opentelemac\examples /E /H /C /I
-xcopy %SRC_DIR%\opentelemac\notebooks %LIBRARY_PREFIX%\opentelemac\notebooks /E /H /C /I
